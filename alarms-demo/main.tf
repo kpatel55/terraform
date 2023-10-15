90371584e536044ed976a41f4d38381fb091bcaf
Kush  Patel <kush@MacBook-Pro.local>
2023-10-14 23:06:39 -0400

    updated sns topic naming

--- alarms-demo/main.tf
+++ alarms-demo/main.tf
@@ -6,16 +6,16 @@ module "alarm_module" {
   source = "./alarms"
 }
 
-resource "aws_sns_topic" "alarm" {
-  name = "high-cpu-topic"
+resource "aws_sns_topic" "sns_notify_alarm" {
+  name = "ec2-alarm-topic"
 }
 
 resource "aws_cloudwatch_composite_alarm" "ec2_alarm" {
   alarm_description = var.alarm_description
   alarm_name = var.alarm_name
 
-  alarm_actions = [aws_sns_topic.alarm.arn]
-  ok_actions = [aws_sns_topic.alarm.arn]
+  alarm_actions = [aws_sns_topic.sns_notify_alarm.arn]
+  ok_actions = [aws_sns_topic.sns_notify_alarm.arn]
 
   alarm_rule = "(ALARM(${module.alarm_module.alarms_out[0]}) OR ALARM(${module.alarm_module.alarms_out[1]})) AND OK(${module.alarm_module.alarms_out[2]})"
 
@@ -27,10 +27,10 @@ resource "aws_cloudwatch_composite_alarm" "ec2_alarm" {
 
   depends_on = [
     module.alarm_module,
-    aws_sns_topic.alarm
+    aws_sns_topic.sns_notify_alarm
   ]
 }
 
 output "sns_out" {
-  value = [module.alarm_module.alarms_out , aws_sns_topic.alarm.arn]
+  value = [module.alarm_module.alarms_out , aws_sns_topic.sns_notify_alarm.arn]
 }
